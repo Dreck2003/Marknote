b3dfdf9a95d95353f568c15c5e54d0d5ca148676
dreck2003 <dikson1.aranda.cabrera@gmail.com>
2022-10-21 15:31:44 -0500

    feat(css): add some utility classes

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,11 @@
-import "./style.css";
+import "./css/root.css";
+import "./css/flex-grid.css";
+import "./css/colors.css";
+import "./css/text.css";
 import App from "./App.svelte";
 
 const app = new App({
-  target: document.getElementById("app"),
+	target: document.getElementById("app"),
 });
 
 export default app;
