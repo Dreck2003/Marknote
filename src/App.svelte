61511bcdcc12be3c958c738aef0f6e5f18ac2ab7
dreck2003 <dikson1.aranda.cabrera@gmail.com>
2022-10-22 10:29:07 -0500

    feat(formatter,linter): add formatter and linter plugins that working with svelte

--- src/App.svelte
+++ src/App.svelte
@@ -1,16 +1,17 @@
 <script lang="ts">
-  import Sidebar from "./lib/components/specific/sidebar/sidebar.svelte";
-
+	import Sidebar from "./lib/components/specific/sidebar/sidebar.svelte";
 </script>
 
 <main class="grid" style="height: 100%;">
-   <Sidebar/>
-   <section class="bg-white-100">code area</section>
+	<Sidebar />
+	<section class="bg-white-100">code area</section>
+	<div />
+	<div />
 </main>
 
 <style>
-   main{
-      grid-template-columns: 200px 1fr;
-      --gap:0;
-   }
+	main {
+		grid-template-columns: 200px 1fr;
+		--gap: 0;
+	}
 </style>
