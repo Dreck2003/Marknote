777d371d6042558c9c42f9768966b6c19b6f86fa
dreck2003 <dikson1.aranda.cabrera@gmail.com>
2022-10-28 10:08:02 -0500

    feat(code-area): add selection and clicked function

--- src/lib/components/common/coder/single_line.svelte
+++ src/lib/components/common/coder/single_line.svelte
@@ -9,7 +9,8 @@
 	});
 </script>
 
-<div class="code_container-lines">
+<div class="code_container-lines" style={$$props.style}>
+	<b class="code_container-shadow" />
 	{#each words as { word, color }}
 		{#if word === " "}
 			<span>&nbsp;</span>
@@ -27,5 +28,16 @@
 	}
 	.code_container-lines {
 		height: 22px;
+		width: max-content;
+		position: relative;
+		background-color: unset;
+	}
+	.code_container-shadow {
+		position: absolute;
+		height: 22px;
+		left: 0;
+		width: 0;
+		background-color: #0030ff17;
+		border-radius: 4px;
 	}
 </style>
